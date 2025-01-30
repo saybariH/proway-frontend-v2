8b1c3a47fc6734b2e9bfbef8e823db8f6207aef1
hamza saybari <saybari.hamza@votreformationpro.com>
2025-01-30 14:49:39 +0100

    create coach layout

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,14 +2,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "../App";
 import React from "react";
+import CoachLayout from "@layouts/Layout";
 const AppRouter : React.FC = () => {
 
 const  router = createBrowserRouter([
     {
         path : "/",
-        element : <App />,
+        element : <CoachLayout/>,
         children : [
-
+             {index : true, element : <App/>}
         ]
     }
 ]);
