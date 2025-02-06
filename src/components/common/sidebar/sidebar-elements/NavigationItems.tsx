ecfa71e7f31421a1f8a973d7a2613a8226d6989a
hamza saybari <saybari.hamza@votreformationpro.com>
2025-02-06 10:21:18 +0100

    side navitems implementation

--- src/components/common/sidebar/sidebar-elements/NavigationItems.tsx
+++ src/components/common/sidebar/sidebar-elements/NavigationItems.tsx
@@ -1,41 +1,63 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { bottomSideBarItems, topSideBarItems } from "src/utils/sideBarItems";
 
 const NavigationItems = () => {
+    const location = useLocation();
     return (
         <>
-            {/* Top sidebar items */}
+            {/* Top sidebar items dashboard, clients, projects and reports */}
             <div className="w-auto max-w-[167px] h-auto flex flex-col gap-2.5">
-                {topSideBarItems.map((item) => (
-                    <Link
-                        to={item.link}
-                        key={item.id}
-                        className="w-[55px] hover:w-[167px] h-[55px] flex items-center group gap-4 rounded-[50px] bg-white bg-opacity-[75%] transition-all duration-300 ease-in-out overflow-hidden"
-                    >
-                        <img src={item.icon} alt="icon" className="w-[20px] h-[20px] ml-4" />
-                        {/* Animated Span (Fade + Slide) */}
-                        <span className="opacity-0 translate-x-[-10px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 delay-150 ease-in-out font-lufga font-medium text-[16px] text-primary">
-                            {item.title}
-                        </span>
-                    </Link>
-                ))}
+                {topSideBarItems.map((item) => {
+                    const isActive = location.pathname === item.link;
+                    return (
+                        <Link
+                            to={item.link}
+                            key={item.id}
+                            className={`w-[55px] hover:w-[167px] h-[55px] flex justify-start  items-center group gap-2.5 rounded-[50px]   transition-all duration-200 ease-in-out 
+                            ${isActive ? 'bg-primary bg-opacity-100' : 'bg-white bg-opacity-[75%]'}`}
+                        >
+                            <img src={item.icon}
+                                alt="icon"
+                                className={`w-[22px] h-[21px] ml-[17px] group-hover:ml-6 transition-all duration-200 delay-150 ease-in-out
+                                            ${isActive ? "filter brightness-0 invert" : "opacity-75"}`} 
+                                            />
+                            {/* Animated Span (Fade + Slide) */}
+                            <span className={`hidden group-hover:inline-block transition-all duration-200 delay-150 ease-in-out font-lufga font-medium text-[16px] 
+                            ${isActive ? 'text-white' : 'text-primary'}`}
+                            >
+                                {item.title}
+                            </span>
+                        </Link>
+                        
+                    )
+                })}
             </div>
 
-            {/* Bottom sidebar items */}
-            <div className="w-full max-w-[167px] h-auto flex flex-col gap-2.5">
-                {bottomSideBarItems.map((item) => (
-                    <Link
-                        to={item.link}
-                        key={item.id}
-                        className="w-[55px] hover:w-[167px] h-[55px] flex items-center group gap-4 rounded-[50px] bg-white bg-opacity-[75%] transition-all duration-300 ease-in-out overflow-hidden"
-                    >
-                        <img src={item.icon} alt="icon" className="w-[20px] h-[20px] ml-4" />
-                        {/* Animated Span (Fade + Slide) */}
-                        <span className="opacity-0 translate-x-[-10px] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 delay-150 ease-in-out font-lufga font-medium text-[16px] text-primary">
-                            {item.title}
-                        </span>
-                    </Link>
-                ))}
+            {/* Bottom sidebar items, support, feedback and settings */}
+            <div className="w-auto max-w-[167px] h-auto flex flex-col gap-2.5">
+                {bottomSideBarItems.map((item) => {
+                    const isActive = location.pathname === item.link;
+                    return (
+                        <Link
+                            to={item.link}
+                            key={item.id}
+                            className={`w-[55px] hover:w-[167px] h-[55px] flex justify-start  items-center group gap-2.5 rounded-[50px]   transition-all duration-200 ease-in-out 
+                            ${isActive ? 'bg-primary bg-opacity-100' : 'bg-white bg-opacity-[75%]'}`}
+                        >
+                            <img src={item.icon}
+                                alt="icon"
+                                className={`w-[22px] h-[21px] ml-[17px] group-hover:ml-6 transition-all duration-200 delay-150 ease-in-out
+                                            ${isActive ? "filter brightness-0 invert" : "opacity-75"}`} 
+                                            />
+                            {/* Animated Span (Fade + Slide) */}
+                            <span className={`hidden group-hover:inline-block transition-all duration-200 delay-150 ease-in-out font-lufga font-medium text-[16px] 
+                            ${isActive ? 'text-white' : 'text-primary'}`}
+                            >
+                                {item.title}
+                            </span>
+                        </Link>
+                    )
+                })}
             </div>
         </>
     );
